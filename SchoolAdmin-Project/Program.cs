e317b24f15bdfe67fc21e3d721d197a427dbb097
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-09-15 21:11:47 +0200

    Add project files.

---
+++ SchoolAdmin-Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolAdmin_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
