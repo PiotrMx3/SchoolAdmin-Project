00705b374cbea0becbc059b06d3b792d5b6919c9
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-09-15 21:38:51 +0200

    SchoolAdmin-Project H10_1 en H10_2 klaar

--- SchoolAdmin-Project/Program.cs
+++ SchoolAdmin-Project/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wat Wil jij doen ?");
+            Console.WriteLine("1: DemonstreerStudenten uitvoren.");
+
+            string k = Console.ReadLine() ?? "";
+
+            switch (k)
+            {
+                case "1":
+                    DemoStudents();
+                    break;
+                default:
+                    Console.WriteLine("Fout maak een nieuwe keuze !");
+                    break;
+            }
+
+        }
+
+        public static void DemoStudents()
+        {
+            Student said = new();
+            said.Name = "Said Aziz";
+            said.Birthdate = new DateTime(2000, 06, 1);
+            said.StudentNmber = Student.StudentCounter;
+
+            Student.StudentCounter++;
+
+            said.Courses = new List<string>() { "Programmeren", "Databanken" };
+
+            Console.WriteLine(said.GenerateNameCard());
+            Console.WriteLine(said.DetermineWorkload());
+
+
+            Student mieke = new();
+            mieke.Name = "Said Aziz";
+            mieke.Birthdate = new DateTime(1998, 1, 1);
+            mieke.StudentNmber = Student.StudentCounter;
+
+            Student.StudentCounter++;
+
+            mieke.Courses = new List<string>() { "Communicatie"};
+            Console.WriteLine(mieke.GenerateNameCard());
+            Console.WriteLine(mieke.DetermineWorkload());
+            
+            
         }
     }
 }
---
+++ SchoolAdmin-Project/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin_Project
+{
+    internal class Student
+    {
+        public static uint StudentCounter = 1;
+
+        public string Name = "";
+        public DateTime Birthdate = new DateTime(1900, 1, 1);
+        public uint StudentNmber = 0;
+        public List<string> Courses = [];
+
+
+        public string GenerateNameCard()
+        {
+            return this.Name + " (Student)";
+        }
+
+        public byte DetermineWorkload()
+        {
+
+            return (byte)(this.Courses.Count * 10);
+        }
+
+    }
+}
