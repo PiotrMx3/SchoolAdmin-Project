e98e1db4ee5e71d0e7e4421737ae2437a9159d51
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-10-13 19:48:36 +0200

    H14_1 SchoolAdmin: Persoon klaar

---
+++ SchoolAdmin-Project/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin_Project
+{
+    internal abstract class Person
+    {
+		private uint _id;
+		private string _name;
+		private DateTime _birthDate;
+		private int _age;
+		private static uint maxId = 1;
+		public abstract string GenerateNameCard();
+		public abstract double DetermineWorkload();
+
+		private static List<Person> _allPersons = new();
+
+		public Person(string name, DateTime birthDate)
+		{
+			this._name = name;
+			this._birthDate = birthDate;
+			this._id = maxId;
+			_allPersons.Add(this);
+
+			maxId++;
+		}
+
+		public static ImmutableList<Person> AllPersons
+		{
+			get { return _allPersons.ToImmutableList<Person>() ; }
+		}
+
+
+        public int Age
+		{
+			get 
+			{
+				DateTime now = DateTime.Now;
+
+				int years = now.Year - Birthdate.Year;
+
+				if (Birthdate.AddYears(years) > now.Date) years--;
+
+				return years;
+			}
+
+		}
+
+
+		public DateTime Birthdate
+		{
+			get { return this._birthDate; }
+		}
+
+
+		public string Name
+		{
+			get { return this._name; }
+			set { this._name = value; }
+		}
+
+		public uint Id
+		{
+			get { return this._id; }
+		}
+
+	}
+}
